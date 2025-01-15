44e1d890849ea5e7579ed33364d0ad3a54027055
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-15 21:58:24 +0100

    Refactor celebration widget: update variable declaration, enhance text styling, and add gradient overlay for improved UI

--- celebration-widget.js
+++ celebration-widget.js
@@ -1,4 +1,4 @@
-const {celebration} = await new Request("https://celebration.pival.fr/api/celebration").loadJSON()
+const celebration = await new Request("https://celebration.pival.fr/api/celebration").loadJSON()
 
 const {
   date,
@@ -17,19 +17,24 @@ w.url = "https://celebration.pival.fr"
 w.backgroundColor = new Color("#222222")
 w.backgroundImage = await new Request(image).loadImage()
 
+// Add a semi-transparent dark overlay
+let gradient = new LinearGradient()
+gradient.colors = [new Color("#000000", 0.3), new Color("#000000", 0.3)]
+gradient.locations = [0.0, 1.0]
+w.backgroundGradient = gradient
 
 w.addSpacer(8)
 
 let staticText = w.addText(date)
 staticText.textColor = Color.white()
-staticText.font = Font.boldSystemFont(12)
+staticText.font = Font.boldSystemFont(24)
 staticText.centerAlignText()
 
-w.addSpacer(8)
+w.addSpacer(15)
 
 let amountTxt = w.addText(name)
-amountTxt.textColor = Color.orange()
-amountTxt.font = Font.systemFont(16)
+amountTxt.textColor = Color.white()
+amountTxt.font = Font.systemFont(24)
 amountTxt.centerAlignText()
 
 Script.setWidget(w)
